6c49dc579b8789a8b6116bfc715c7f96c40ddae1
AhmedSamy02 <nname1858@gmail.com>
2025-08-09 16:38:24 +0300

    [🐞 BUG] Fixing error state bug

--- app/src/main/java/com/example/fakestore/utils/State.kt
+++ app/src/main/java/com/example/fakestore/utils/State.kt
@@ -3,5 +3,5 @@ package com.example.fakestore.utils
 sealed class State<out T> {
     data object Loading : State<Nothing>()
     data class Success<T>(val data: T) : State<T>()
-    data class Error<String>(val message:String):State<String>()
+    data class Error<String>(val message:String):State<Nothing>()
 }
