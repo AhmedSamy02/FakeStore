949f43d5c70d842fb60f19e7aec8f3fba0d013ca
AhmedSamy02 <nname1858@gmail.com>
2025-08-09 19:35:48 +0300

    [🐞 BUG] Fixing project doesn't build

---
+++ app/src/main/java/com/example/fakestore/uis/Categories.kt
@@ -0,0 +1,15 @@
+package com.example.fakestore.uis
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun CategoriesScreen(){
+CategoriesItem()
+}
+@Composable
+fun CategoriesItem(){
+    Text("Hello")
+}
